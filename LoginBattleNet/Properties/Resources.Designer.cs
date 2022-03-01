0b3c07dd1c62287ffd86a60c334274766d6a3ebe
Danil Vetrov <danil-vetrov47@mail.ru>
2022-03-01 23:32:59 +0400

    все разметил, добавил картинки в толбар и лого.

--- LoginBattleNet/Properties/Resources.Designer.cs
+++ LoginBattleNet/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace LoginBattleNet.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap logo {
+            get {
+                object obj = ResourceManager.GetObject("logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap крест_актив {
             get {
                 object obj = ResourceManager.GetObject("крест_актив", resourceCulture);
